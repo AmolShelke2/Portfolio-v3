b67ab8ec9347cc42dfcbd8cd98d00720ac3c3947
Amol shelke <shelkeamol897@gmail.com>
2023-02-21 18:12:42 +0530

    Added custom font and font family

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 function App() {
   return (
     <div className="App">
-      <h1 className=" text-black text-center text-3xl my-5 uppercase">Hello</h1>
+      <h1 className=" text-black text-center text-3xl my-5">Hello</h1>
     </div>
   );
 }
