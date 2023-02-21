3041aa241f3a4d2f8512d9fed0a817e75cab006e
Amol shelke <shelkeamol897@gmail.com>
2023-02-21 21:39:58 +0530

    Adding Navbar component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import logo from './logo.svg';
 
 function App() {
-  return (
-    <div className="App">
-      <h1 className=" text-black text-center text-4xl my-5 font-signature">
-        Hello
-      </h1>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
