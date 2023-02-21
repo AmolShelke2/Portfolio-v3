9ba142c9998370de43ff7c149e5d6468a5f4164d
Amol shelke <shelkeamol897@gmail.com>
2023-02-22 00:15:12 +0530

    Updated Navbar with hamburger menu feture

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { FaBars, FaTimes } from 'react-icons/fa';
 
 const NavBar = () => {
+  const [nav, setNav] = useState(false);
+
   const links = [
     {
       id: 1,
@@ -51,9 +53,30 @@ const NavBar = () => {
         ))}
       </ul>
 
-      <div className=" cursor-pointer pr-4 z-10 text-gray-500">
-        <FaBars size={30} />
+      <div
+        className=" cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
+        onClick={() => setNav(!nav)}
+      >
+        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
       </div>
+
+      {nav && (
+        <ul
+          className="flex flex-col justify-center items-center 
+        absolute top-0 left-0 w-full h-screen bg-gradient-to-b
+         from-black to-gray-800 text-gray-500"
+        >
+          {links.map(({ id, link }) => (
+            <li
+              className=" px-4 cursor-pointer capitalize py-6
+            text-4xl"
+              key={id}
+            >
+              {link}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
