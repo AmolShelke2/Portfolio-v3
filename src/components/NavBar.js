effe5a7f94fbb21ffacc215c88d94550e9c3c781
Amol shelke <shelkeamol897@gmail.com>
2023-02-21 23:38:33 +0530

    Added links dynamically

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,57 @@
 import React from 'react';
 
+import { faBars, faTimes } from 'react-icons/fa';
+
 const NavBar = () => {
-  return <div>NavBar</div>;
+  const links = [
+    {
+      id: 1,
+      link: 'Home',
+    },
+
+    {
+      id: 2,
+      link: 'About',
+    },
+
+    {
+      id: 3,
+      link: 'Portfolio',
+    },
+
+    {
+      id: 4,
+      link: 'Experience',
+    },
+
+    {
+      id: 5,
+      link: 'Contact',
+    },
+  ];
+
+  return (
+    <div
+      className="flex justify-between items-center w-full h-20 px-5
+     text-white bg-black fixed"
+    >
+      <div>
+        <h1 className="text-5xl font-signature ml-2">Amol</h1>
+      </div>
+
+      <ul className="flex">
+        {links.map(link => (
+          <li
+            className="px-4 cursor-pointer capitalize font-medium
+         text-gray-500 hover:scale-105 duration-200"
+            key={link.id}
+          >
+            {link.link}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default NavBar;
