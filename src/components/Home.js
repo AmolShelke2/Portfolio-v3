97375fcf5dcd496280422f83c218613231ae32fe
Amol shelke <shelkeamol897@gmail.com>
2023-02-23 01:21:19 +0530

    Update home component with styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -25,10 +25,14 @@ const Home = () => {
           </p>
 
           <div>
-            <button className="text-white">
+            <button
+              className="group text-white w-fit px-6 py-3 my-2
+             flex items-center rounded-md bg-gradient-to-r
+              from-cyan-500 to-blue-500 cursor-pointer"
+            >
               Portfolio{' '}
-              <span>
-                <MdOutlineKeyboardArrowRight />
+              <span className="group-hover:rotate-90 duration-300">
+                <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
               </span>
             </button>
           </div>
