cec9a1b9035dcf0ba55674f6e67f95417b2c142e
Amol shelke <shelkeamol897@gmail.com>
2023-02-22 23:34:38 +0530

    Adding tailwind styling for home section

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,31 @@ import React from 'react';
 
 import heroImage from '../assets/myHeroImage.jpg';
 
+import { MdOutlineKeyboardArrowRight } from 'react-icons/md';
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div name="home">
+      <div>
+        <div>
+          <h2>I'm a front End Developer</h2>
+          <p>
+            I have 1 year of experince building and designing front end websites
+            from scratch. Currently I love to work on web applications using
+            technologies like React, Tailwind❤️
+          </p>
+
+          <div>
+            <button>
+              Portfolio{' '}
+              <span>
+                <MdOutlineKeyboardArrowRight />
+              </span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
