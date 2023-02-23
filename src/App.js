01a315675fd7e09d78e38ca30acd92421b27c734
Amol shelke <shelkeamol897@gmail.com>
2023-02-23 20:58:02 +0530

    added About component with Tailwind css styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import Home from './components/Home';
-import NavBar from './components/NavBar';
-import SocialLinks from './components/SocialLinks';
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import SocialLinks from "./components/SocialLinks";
+import About from "./components/About";
 
 function App() {
   return (
     <div>
       <NavBar />
       <Home />
+      <About />
 
       <SocialLinks />
     </div>
--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,40 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b
+       from-gray-800 to-black text-white"
+    >
+      <div
+        className="max-w-screen-lg mx-auto flex flex-col 
+        justify-center w-full h-full"
+      >
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
+        </div>
+        <p className="text-xl mt-20">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi magnam
+          consequatur quae ea quam doloribus, sit aperiam impedit magni sint
+          maxime, enim error itaque quisquam pariatur? Officia minus doloremque
+          qui delectus illum. Enim, sit maxime laboriosam, necessitatibus cum in
+          maiores ad perferendis quam vitae explicabo nihil at rerum error! Nam?
+        </p>
+        <br />
+        <p className="text-xl">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
+          maxime eaque impedit reprehenderit, minima nihil, ab cupiditate porro
+          voluptatibus aut delectus iusto officiis voluptates maiores vel?
+          Voluptatum, odit esse nam officiis aliquid commodi assumenda,
+          cupiditate obcaecati excepturi tempora unde repudiandae perspiciatis
+          illo minima quae maiores. Quis necessitatibus fuga eum illo?
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default About;
