4ac0c87ec9b033b6083f262b9e45fd146d0cab49
Amol shelke <shelkeamol897@gmail.com>
2023-02-23 20:38:50 +0530

    Added SocialLinks component with Tailwind css styling

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -1,8 +1,8 @@
-import React, { Fragment } from 'react';
+import React, { Fragment } from "react";
 
-import { FaGithub, FaLinkedin } from 'react-icons/fa';
-import { HiOutlineMail } from 'react-icons/hi';
-import { BsFillPersonLinesFill } from 'react-icons/bs';
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
 
 const SocialLinks = () => {
   const links = [
@@ -13,8 +13,8 @@ const SocialLinks = () => {
           Linkedin <FaLinkedin size={30} />
         </>
       ),
-      href: 'https://www.linkedin.com/in/amol-shelke-627813220/',
-      style: 'rounded-tr-md',
+      href: "https://www.linkedin.com/in/amol-shelke-627813220/",
+      style: "rounded-tr-md",
     },
 
     {
@@ -24,7 +24,7 @@ const SocialLinks = () => {
           Github <FaGithub size={30} />
         </>
       ),
-      href: 'https://github.com/AmolShelke2',
+      href: "https://github.com/AmolShelke2",
     },
 
     {
@@ -34,7 +34,7 @@ const SocialLinks = () => {
           Linkedin <FaLinkedin size={30} />
         </>
       ),
-      href: 'https://www.linkedin.com/in/amol-shelke-627813220/',
+      href: "https://www.linkedin.com/in/amol-shelke-627813220/",
     },
 
     {
@@ -44,40 +44,46 @@ const SocialLinks = () => {
           Mail <HiOutlineMail size={30} />
         </>
       ),
-      href: 'mailto:shelkeamol829@gmail.com',
+      href: "mailto:shelkeamol829@gmail.com",
     },
 
     {
       id: 4,
       child: (
         <>
-          Linkedin <BsFillPersonLinesFill size={30} />
+          Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: '/resume.pdf',
-      style: 'rounded-br-md',
+      href: "/resume.pdf",
+      style: "rounded-br-md",
       download: true,
     },
   ];
 
   return (
-    <div className="flex flex-col top-[35%] left-0 fixed">
+    <div className="flex flex-col top-[35%] left-0 fixed hidden lg:flex">
       <ul>
-        <li
-          className="flex justify-between items-center w-40 
-        h-14 px-4 ml-[-100px] hover:rounded-md hover:ml-[10px] 
-        duration-300 bg-gray-500"
-        >
-          <a
-            href=""
-            className="flex justify-between items-center 
-            w-full text-white"
+        {links.map(({ id, child, href, style, download }) => (
+          <li
+            key={id}
+            className={
+              "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:rounded-md hover:ml-[10px] duration-300 bg-gray-500" +
+              " " +
+              style
+            }
           >
-            <>
-              Linkedin <FaLinkedin size={30} />
-            </>
-          </a>
-        </li>
+            <a
+              href={href}
+              className="flex justify-between items-center 
+            w-full text-white"
+              download={download}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {child}
+            </a>
+          </li>
+        ))}
       </ul>
     </div>
   );
