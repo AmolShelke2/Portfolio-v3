98fbe2c50b3495eecdfcc359304a3ac2268181c5
Amol shelke <shelkeamol897@gmail.com>
2023-02-24 21:06:22 +0530

    Updated

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,5 +1,12 @@
 import React from "react";
 
+import covid from "../assets/projects/covid.png";
+import discord from "../assets/projects/DIscord.png";
+import eCommerce from "../assets/projects/ecommerce.png";
+import portfolio from "../assets/projects/portfolio.png";
+import todo from "../assets/projects/todo.png";
+import weather from "../assets/projects/weather.png";
+
 const Project = () => {
   return <div>Project</div>;
 };
