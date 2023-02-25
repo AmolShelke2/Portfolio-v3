032a942e768543d457389c19ad6f241c8eb58aab
Amol shelke <shelkeamol897@gmail.com>
2023-02-25 12:03:44 +0530

    Updated

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,11 +1,26 @@
 import React from "react";
 
+import html from "../assets/html.png";
+import css from "../assets/css.png";
+import github from "../assets/github.png";
+import javascript from "../assets/javascript.png";
+import reactIcon from "../assets/react.png";
+import nextjsIcon from "../assets/nextjs.png";
+import nodeIcon from "../assets/node.png";
+import tailwindIcon from "../assets/tailwind.png";
+
 const Skills = () => {
   return (
-    <div>
-      <div>
-        <p>Sills</p>
-        <p>Tech stack on which I've worked</p>
+    <div
+      name="skills"
+      className="bg-gradient-to-b from-black to-gray-800 text-white
+      w-full md:h-screen"
+    >
+      <div className="grid items-center">
+        <div>
+          <p>Sills</p>
+          <p>Tech stack on which I've worked</p>
+        </div>
       </div>
     </div>
   );
