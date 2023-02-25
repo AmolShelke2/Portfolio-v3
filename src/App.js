36d087abdc25bcaefa2b2d2ac5169ad6ae898ca2
Amol shelke <shelkeamol897@gmail.com>
2023-02-25 11:42:41 +0530

    Adding skills component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from "./components/NavBar";
 import SocialLinks from "./components/SocialLinks";
 import About from "./components/About";
 import Project from "./components/Project";
+import Skills from "./components/Skills";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Home />
       <About />
       <Project />
+      <Skills />
 
       <SocialLinks />
     </div>
---
+++ src/components/Skills.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Skills = () => {
+  return (
+    <div>
+      <div>
+        <p>Sills</p>
+        <p>Tech stack on which I've worked</p>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
