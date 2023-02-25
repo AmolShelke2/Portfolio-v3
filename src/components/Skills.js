b8d77134e1a67d5a4e6e954370a2dcc1be24b028
Amol shelke <shelkeamol897@gmail.com>
2023-02-26 00:27:14 +0530

    Added Skills component

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import html from "../assets/html.png";
 import css from "../assets/css.png";
+import git from "../assets/git.png";
 import github from "../assets/github.png";
 import javascript from "../assets/javascript.png";
 import reactIcon from "../assets/react.png";
@@ -10,6 +11,71 @@ import nodeIcon from "../assets/node.png";
 import tailwindIcon from "../assets/tailwind.png";
 
 const Skills = () => {
+  const skills = [
+    {
+      id: 1,
+      src: html,
+      title: "HTML",
+      style: "shadow-orange-500",
+    },
+
+    {
+      id: 2,
+      src: css,
+      title: "CSS",
+      style: "shadow-blue-500",
+    },
+
+    {
+      id: 3,
+      src: javascript,
+      title: "JavaScript",
+      style: "shadow-yellow-500",
+    },
+
+    {
+      id: 4,
+      src: reactIcon,
+      title: "React JS",
+      style: "shadow-blue-600",
+    },
+
+    {
+      id: 5,
+      src: tailwindIcon,
+      title: "Tailwind",
+      style: "shadow-sky-400",
+    },
+
+    {
+      id: 6,
+      src: nextjsIcon,
+      title: "Next JS",
+      style: "shadow-white",
+    },
+
+    {
+      id: 7,
+      src: nodeIcon,
+      title: "Node",
+      style: "shadow-green-500",
+    },
+
+    {
+      id: 8,
+      src: github,
+      title: "Github",
+      style: "shadow-gray-400",
+    },
+
+    {
+      id: 9,
+      src: git,
+      title: "Git",
+      style: "shadow-orange-500",
+    },
+  ];
+
   return (
     <div
       name="skills"
@@ -34,13 +100,16 @@ const Skills = () => {
           className="w-full grid grid-cols-2 sm:grid-cols-3
         gap-8 text-center py-8 px-12 sm:px-0"
         >
-          <div
-            className="shadow-md hover:scale-105 
-          duration-500 py-2 rounded-lgs"
-          >
-            <img src={html} alt="" className="w-20 mx-auto" />
-            <p className="mt-4">HTML</p>
-          </div>
+          {skills.map(({ id, src, title, style }) => (
+            <div
+              key={id}
+              className={`shadow-md hover:scale-105 
+          duration-500 py-2 rounded-lg ${style}`}
+            >
+              <img src={src} alt="skills-images" className="w-20 mx-auto" />
+              <p className="mt-5 text-md">{title}</p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
