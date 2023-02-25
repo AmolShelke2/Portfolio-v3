edb4c478afd22a3ba1327e799791b0d26e0d2d6c
Amol shelke <shelkeamol897@gmail.com>
2023-02-25 23:51:06 +0530

    Adding skills dynamically using the map

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -13,13 +13,34 @@ const Skills = () => {
   return (
     <div
       name="skills"
-      className="bg-gradient-to-b from-black to-gray-800 text-white
+      className="bg-gradient-to-b from-gray-800 to-black text-white
       w-full md:h-screen"
     >
-      <div className="grid items-center">
+      <div
+        className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center
+       w-full h-full text-white"
+      >
         <div>
-          <p>Sills</p>
-          <p>Tech stack on which I've worked</p>
+          <p
+            className="text-4xl font-bold border-b-4 border-gray-500 
+          p-2 inline"
+          >
+            Skills
+          </p>
+          <p className="py-6">Tech stack on which I've worked</p>
+        </div>
+
+        <div
+          className="w-full grid grid-cols-2 sm:grid-cols-3
+        gap-8 text-center py-8 px-12 sm:px-0"
+        >
+          <div
+            className="shadow-md hover:scale-105 
+          duration-500 py-2 rounded-lgs"
+          >
+            <img src={html} alt="" className="w-20 mx-auto" />
+            <p className="mt-4">HTML</p>
+          </div>
         </div>
       </div>
     </div>
