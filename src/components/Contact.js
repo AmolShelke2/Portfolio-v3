9c28aee69b1af3d258f82486255118a7ad4214e3
Amol shelke <shelkeamol897@gmail.com>
2023-02-26 20:37:54 +0530

    styled Contact Component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,15 +2,27 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div>
-      <div>
-        <div>
-          <p>Contact</p>
-          <p>Submit the form below to get in touch with me!</p>
+    <div
+      name="contact"
+      className="bg-gradient-to-b from-black
+     to-gray-800 w-full h-screen text-white"
+    >
+      <div
+        className="flex flex-col p-4 justify-center
+       max-w-screen-lg h-full mx-auto"
+      >
+        <div className="pb-8">
+          <p
+            className="text-4xl font-bold border-b-4 border-gray-500
+           pb-2 inline"
+          >
+            Contact
+          </p>
+          <p className="py-6">Submit the form below to get in touch with me!</p>
         </div>
 
-        <div>
-          <form action="">
+        <div className="flex justify-center items-center">
+          <form action="" className="flex flex-col w-full md:w-1/2">
             <input
               type="text"
               placeholder="Enter your name"
@@ -23,13 +35,14 @@ const Contact = () => {
               type="email"
               name="email"
               placeholder="Enter your email"
-              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+              className="my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
             />
 
             <textarea
               name="message"
               cols="30"
               rows="10"
+              placeholder="Enter your message"
               className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
             ></textarea>
 
