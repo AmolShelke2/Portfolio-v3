1c417cc1cd8b670b5aab99ec4e210cffbfc90384
Amol shelke <shelkeamol897@gmail.com>
2023-02-27 22:07:58 +0530

    Adding Experience Slider component

---
+++ src/components/Experience.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const Experience = () => {
+  return <div>Experience</div>;
+};
+
+export default Experience;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -24,7 +24,7 @@ const NavBar = () => {
 
     {
       id: 4,
-      link: "Experience",
+      link: "Skills",
     },
 
     {
