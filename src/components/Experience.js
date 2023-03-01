7af060b34a3bbd63a8b05db9d94ee720c2c66750
Amol shelke <shelkeamol897@gmail.com>
2023-03-02 00:43:50 +0530

    adding styles with material tailwind

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -76,47 +76,7 @@ const Experience = () => {
         <div
           className="flex flex-row justify-center items-center 
         md:flex-wrap lg:flex-wrap"
-        >
-          <div className="h-80 w-80">
-            <div>
-              <img src={codedamn} alt="codedamn" />
-            </div>
-            <div>
-              <h2>Codedamn</h2>
-              <h3>Tech Support team</h3>
-              <h4>nov-022 to feb-023</h4>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Laborum, animi? Quod iure ad recusandae veniam.
-              </p>
-              <ul>
-                <li>Lorem ipsum dolor sit amet.</li>
-                <li>Lorem ipsum dolor sit amet.</li>
-                <li>Lorem ipsum dolor sit amet.</li>
-              </ul>
-            </div>
-          </div>
-
-          <div className="h-80 w-80">
-            <div>
-              <img src={codedamn} alt="codedamn" />
-            </div>
-            <div>
-              <h2>Codedamn</h2>
-              <h3>Tech Support team</h3>
-              <h4>nov-022 to feb-023</h4>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Laborum, animi? Quod iure ad recusandae veniam.
-              </p>
-              <ul>
-                <li>Lorem ipsum dolor sit amet.</li>
-                <li>Lorem ipsum dolor sit amet.</li>
-                <li>Lorem ipsum dolor sit amet.</li>
-              </ul>
-            </div>
-          </div>
-        </div>
+        ></div>
       </div>
     </div>
   );
