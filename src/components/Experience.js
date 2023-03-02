1ebeb8568221771e321857f25a36d8859f7872f6
Amol shelke <shelkeamol897@gmail.com>
2023-03-02 23:10:41 +0530

    feat: done creating experience card component

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -5,7 +5,6 @@ import "aos/dist/aos.css";
 
 import codedamn from "../assets/experience/codedamn.png";
 import girlscript from "../assets/experience/girlscript.jpg";
-import hundredDaysOfCode from "../assets/experience/100daysofcode.jpg";
 
 const Experience = () => {
   useEffect(() => {
@@ -17,39 +16,20 @@ const Experience = () => {
       company: "codedamn",
       role: "Tech Support team",
       date: "Nov-022 to Feb-023",
+      logo: codedamn,
       description:
-        "Codedamn is a E-learning platform where coders can learn and apply there skills practically",
-      tasks: {
-        task1: "Writen blogs on various technologies",
-        task2: "Become an top performer and get extended for 6 months",
-        task3: "Written plag free articles",
-      },
+        "Codedamn is a E-learning platform where coders can learn and apply there skills practically. I work there as a technical content writer and written multiple blogs on various technologies",
+      topics: ["writing", "research", "frontend"],
     },
 
     {
       company: "Girlscript foundation",
       role: "Open-source developer",
       date: "Feb-022 to May-022",
+      logo: girlscript,
       description:
-        "Girlscript foundation is an non-profitable organization which provides free open-source coding opportunities.",
-      tasks: {
-        task1: "Created contributions over 15+ github repositories",
-        task2: "Become a top performer with 15th rank all over India",
-        task3: "Created all the contributions without any plagiarisms",
-      },
-    },
-
-    {
-      company: "#100daysofcode",
-      role: "Self-tought developer",
-      date: "Dec-021 to Mar-022",
-      description:
-        "#100daysofcode is an challenging way of learning and posting your progress with other as a self-tought developer",
-      tasks: {
-        task1: "Started Learning web development with zero knowldge",
-        task2: "Learned HTML, CSS and JavaScript in this challenge",
-        task3: "Created and deployed multiple projects with github and vercel",
-      },
+        "Girlscript foundation is an non-profitable organization which provides free open-source coding opportunities. I work at Girlscript foundation as a open-source contributor and in the period of 3 months I have done more than 60+ contributions.",
+      topics: ["webdev", "frontend", "open-source"],
     },
   ];
 
@@ -62,7 +42,7 @@ const Experience = () => {
     >
       <div
         className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center
-        text-white md:flex-wrap lg:flex-wrap"
+        text-white"
       >
         <div>
           <p
@@ -74,10 +54,44 @@ const Experience = () => {
           <p className="my-8">Here is my work experience</p>
         </div>
 
-        <div
-          className="flex flex-row justify-center items-center 
-        md:flex-wrap lg:flex-wrap"
-        ></div>
+        <div className="flex flex-row justify-center items-center flex-wrap">
+          {experinces.map(experience => (
+            <div className="max-w-sm rounded overflow-hidden shadow-lg shadow-gray-600 mx-6 mb-4 lg:my-6">
+              <img className="w-full" src={experience.logo} alt="Mountain" />
+              <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-1">
+                  {experience.company}
+                </div>
+                <div className="font-normal text-lg mb-1">
+                  {experience.date}
+                </div>
+                <p className="text-gray-700 text-base">
+                  {experience.description}
+                </p>
+              </div>
+              <div className="px-6 pt-4 pb-2">
+                <span
+                  className="inline-block bg-gray-200 rounded-full px-3 py-1
+               text-sm font-semibold text-gray-700 mr-2 mb-2"
+                >
+                  #{experience.topics[0]}{" "}
+                </span>
+                <span
+                  className="inline-block bg-gray-200 rounded-full px-3 py-1
+               text-sm font-semibold text-gray-700 mr-2 mb-2"
+                >
+                  #{experience.topics[1]}{" "}
+                </span>
+                <span
+                  className="inline-block bg-gray-200 rounded-full px-3 py-1 
+              text-sm font-semibold text-gray-700 mr-2 mb-2"
+                >
+                  #{experience.topics[2]}{" "}
+                </span>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
